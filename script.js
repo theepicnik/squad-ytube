bba107412add4ab264218304247468f4897bf1dd
Nikhil Tamhankar <iamnik.mailme@gmail.com>
2023-08-14 14:34:17 +0530

    commit #3

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
+// ... (previous code)
+
 document.getElementById("watchButton").addEventListener("click", function() {
-    const videoUrls = [
-      document.getElementById("videoUrl1").value,
-      document.getElementById("videoUrl2").value,
-      document.getElementById("videoUrl3").value,
-      document.getElementById("videoUrl4").value
-    ];
+  // ... (previous code)
+
+  const iframes = [];
+
+  for (let i = 1; i <= 4; i++) {
+    const iframe = document.createElement("iframe");
+    iframe.src = generateEmbedUrl(videoUrls[i - 1]);
+    iframe.setAttribute("allowfullscreen", "");
+    document.getElementById(`video${i}`).innerHTML = "";
+    document.getElementById(`video${i}`).appendChild(iframe);
+    
+    iframes.push(iframe);
+  }
+
+  synchronizeVideos(iframes);
+});
+
+// Function to synchronize videos based on audio
+function synchronizeVideos(iframes) {
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const analyzers = [];
   
-    for (let i = 1; i <= 4; i++) {
-      const iframe = document.createElement("iframe");
-      iframe.src = generateEmbedUrl(videoUrls[i - 1]);
-      iframe.setAttribute("allowfullscreen", "");
-      document.getElementById(`video${i}`).innerHTML = "";
-      document.getElementById(`video${i}`).appendChild(iframe);
+  // Create analyzers for each video's audio
+  for (let i = 0; i < iframes.length; i++) {
+    const analyzer = audioContext.createAnalyser();
+    analyzers.push(analyzer);
+    
+    const iframeWindow = iframes[i].contentWindow;
+    iframeWindow.addEventListener("load", function() {
+      const iframeAudio = new Audio();
+      iframeAudio.src = iframes[i].src.replace("/embed/", "/watch?v="); // Convert to regular YouTube link
+      const source = audioContext.createMediaElementSource(iframeAudio);
+      source.connect(analyzer);
+      analyzer.connect(audioContext.destination);
+      iframeAudio.play();
+    });
+  }
+  
+  // Synchronize the analyzers
+  setInterval(function() {
+    const frequencies = analyzers.map(analyzer => {
+      const dataArray = new Uint8Array(analyzer.frequencyBinCount);
+      analyzer.getByteFrequencyData(dataArray);
+      return dataArray;
+    });
+    
+    const averageFrequencies = frequencies.map(array => {
+      const sum = array.reduce((acc, value) => acc + value, 0);
+      return sum / array.length;
+    });
+    
+    const referenceFrequency = averageFrequencies[0];
+    const maxDeviation = 10; // Adjust this value for sensitivity
+    
+    for (let i = 1; i < iframes.length; i++) {
+      const frequencyDiff = Math.abs(referenceFrequency - averageFrequencies[i]);
+      if (frequencyDiff > maxDeviation) {
+        iframes[i].contentWindow.postMessage("pause", "*"); // Pause out-of-sync videos
+      } else {
+        iframes[i].contentWindow.postMessage("play", "*"); // Play in-sync videos
+      }
     }
-  });
-  
-  function generateEmbedUrl(url) {
-    // Extract the video ID from the YouTube URL
-    const videoId = getYouTubeVideoId(url);
-    // Construct the embed URL
-    return `https://www.youtube.com/embed/${videoId}`;
-  }
-  
-  function getYouTubeVideoId(url) {
-    const regex = /[?&]v=([^&#]*)/;
-    const match = regex.exec(url);
-    return match && match[1];
-  }
-  
+  }, 100); // Adjust interval as needed
+}
