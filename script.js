b4561d3638cf2e26f8aed93a786d2d7ed63f8d3f
Nikhil Tamhankar <iamnik.mailme@gmail.com>
2023-08-14 14:56:54 +0530

    commit #7

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 document.getElementById("watchButton").addEventListener("click", function() {
-    const videoUrls = [
-      document.getElementById("videoUrl1").value,
-      document.getElementById("videoUrl2").value,
-      document.getElementById("videoUrl3").value,
-      document.getElementById("videoUrl4").value
-    ];
-  
-    for (let i = 1; i <= 4; i++) {
-      const iframe = document.createElement("iframe");
-      iframe.src = generateEmbedUrl(videoUrls[i - 1]);
-      iframe.setAttribute("allowfullscreen", "");
-      document.getElementById(`video${i}`).innerHTML = "";
-      document.getElementById(`video${i}`).appendChild(iframe);
-    }
-  });
-  
-  function generateEmbedUrl(url) {
-    // Extract the video ID from the YouTube URL
-    const videoId = getYouTubeVideoId(url);
-    // Construct the embed URL
-    return `https://www.youtube.com/embed/${videoId}`;
+  const videoUrls = [
+    document.getElementById("videoUrl1").value,
+    document.getElementById("videoUrl2").value,
+    document.getElementById("videoUrl3").value,
+    document.getElementById("videoUrl4").value
+  ];
+
+  for (let i = 1; i <= 4; i++) {
+    const iframe = document.createElement("iframe");
+    iframe.src = generateEmbedUrl(videoUrls[i - 1]);
+    iframe.setAttribute("allowfullscreen", "");
+    document.getElementById(`video${i}`).innerHTML = "";
+    document.getElementById(`video${i}`).appendChild(iframe);
   }
-  
-  function getYouTubeVideoId(url) {
-    const regex = /[?&]v=([^&#]*)/;
-    const match = regex.exec(url);
-    return match && match[1];
-  }
-  
+});
+
+function generateEmbedUrl(url) {
+  // Extract the video ID from the YouTube URL
+  const videoId = getYouTubeVideoId(url);
+  // Construct the embed URL
+  return `https://www.youtube.com/embed/${videoId}`;
+}
+
+function getYouTubeVideoId(url) {
+  const regex = /[?&]v=([^&#]*)/;
+  const match = regex.exec(url);
+  return match && match[1];
+}
